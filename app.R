2e696f178483b7a687bafee5014e11eae946b6f2
Cale Weaver <Jcaleweaver@gmail.com>
2020-05-03 15:15:44 +0200

    Add shiny app

---
+++ app.R
@@ -0,0 +1,31 @@
+library(leaflet)
+library(shiny)
+library(sp)
+
+data <- read.csv("./euroTravels/data/cities.csv")
+
+data$long <- as.numeric(data$long)
+data$lat <- as.numeric(data$lat)
+
+passportIcon <- makeIcon(
+  iconUrl = "https://img.icons8.com/offices/30/000000/passport.png",
+  iconAnchorX = 15, iconAnchorY = 15
+)
+
+ui <- fluidPage(fillPage(tags$style(type = "text/css",
+                                    "#mymap {height: 100vh !important;
+                                    width: 100vw !important;
+                                    margin-left: -15px; !important;}"),
+                         leafletOutput("mymap", width = "100%",height = "100%")
+)
+)
+server <- function(input, output) {
+  output$mymap <- renderLeaflet({ leaflet() %>%
+      addTiles() %>%
+      addMarkers(data = data, lng = ~long, lat = ~lat,icon = passportIcon, popup = ~city) %>%
+      addProviderTiles(providers$Esri.NatGeoWorldMap)
+    
+  })
+}
+
+shinyApp(ui, server)
