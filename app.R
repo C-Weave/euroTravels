33b408b44fefd6efee4c97f0f738eeb87f002879
Cale Weaver <Jcaleweaver@gmail.com>
2020-05-15 05:15:47 +0200

    Add comments

--- app.R
+++ app.R
@@ -2,11 +2,14 @@ library(leaflet)
 library(shiny)
 library(sp)
 
+# read in cities data
 data <- read.csv("data/cities.csv")
 
+# set long and lat as numerics
 data$long <- as.numeric(data$long)
 data$lat <- as.numeric(data$lat)
 
+# create passport icon to use as marker
 passportIcon <- makeIcon(
   iconUrl = "https://img.icons8.com/offices/30/000000/passport.png",
   iconAnchorX = 15, iconAnchorY = 15
